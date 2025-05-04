3713f76b407ba94d9d4473e2b02e46eac70edb32
Arturooiwnl <andresperotto71@gmail.com>
2025-05-03 22:01:33 -0300

    add first version

---
+++ src/layouts/constLayout.ts
@@ -0,0 +1,3 @@
+export const description = "Una galería de componentes UI facíl de usar para tus sitios webs hechos con tailwindCSS"
+export const siteName = "UiComponents"
+export const imageMetaAlt = "UiComponents | Galería de componentes UI facíl de usar para tus sitios webs hechos con tailwindCSS"
